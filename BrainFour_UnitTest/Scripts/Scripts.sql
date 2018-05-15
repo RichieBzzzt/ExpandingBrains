c78585b5189b84fef1a72b14bad749dae9048122
Richie Lee <richie@bzzzt.io>
2018-05-15 10:18:14 +0100

    upload

---
+++ BrainFour_UnitTest/Scripts/Scripts.sql
@@ -0,0 +1,19 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+
+/*POST DEPLOY SCRIPT*/
+
+raiserror('Brain Four Unit Test - reference original script!', 1,1);
+
+:r ..\..\BrainFour\Scripts\Scripts.sql
+GO
---
+++ BrainTwo/Scripts/Scripts.sql
@@ -0,0 +1,16 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+
+/*POST DEPLOY SCRIPT*/
+
+raiserror('Brain Two - Manually copy over from one project to another', 1,1);
